c091bd5b5d4e17e0b79a39693aaa1842f070808f
Nikhil Shetty <shettynikhil1903@gmail.com>
2022-11-02 14:51:47 +0530

    Added another route and Controller for user profiles

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-
+// const userController = require('../controllers/users_controller');
 
 
 
 // console.log('router loaded');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
 
 module.exports = router;
