37fab5718abaf136455da3246796f432fe09222a
Nikhil Shetty <shettynikhil1903@gmail.com>
2022-11-12 12:21:16 +0530

    Added posts and displaying the respective users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,25 @@
+const Post = require('../models/post');
+const user = require('../models/user');
+
 module.exports.home = function (req, res) {
-    console.log(req.cookies);
-    return res.render('home', {
-        title: "Home"
-    });
+    // console.log(req.cookies);
+
+    // Post.find({}, function (err, posts) {
+
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts    
+    //     });
+    // });
+
+    // Populating user of each post
+
+    Post.find({}).populate('user').exec(function (err, posts) {
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        })
+    })
+
+
 }
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
-
+router.use('/posts', require('./posts'));
 
 module.exports = router;
