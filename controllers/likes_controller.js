620bad0963a72ae47215aea6a21ff4e37efe8759
Nikhil Shetty <shettynikhil1903@gmail.com>
2022-12-07 21:05:45 +0530

    Added working functionality for liking posts and comments

--- controllers/likes_controller.js
+++ controllers/likes_controller.js
@@ -9,15 +9,17 @@ module.exports.toggleLike = async function (req, res) {
         let likeable;
         let deleted = false;
 
-
-        if (req.query.type = post) {
+        // if (req.query.type ='post') {
+        // yha  compare hie galat aya h,schema Post hoga apne ne value assign kar raka tha 
+        if (req.query.type == 'Post') {
             likeable = await Post.findById(req.query.id).populate('likes');
         } else {
             likeable = await Comment.findById(req.query.id).populate('likes');
         }
 
         // Check if a like already exists 
-        let existingLike = await Like.findById({
+        // find by Id nhi karna 
+        let existingLike = await Like.findOne({
             likeable: req.query.id,
             onModel: req.query.type,
             user: req.user._id
@@ -30,17 +32,19 @@ module.exports.toggleLike = async function (req, res) {
             likeable.save();
 
             existingLike.remove();
-            deleted: true;
+            deleted = true;
         }
         // If doesn't exit, add a like 
         else {
-            let newLike = await new Like.create({
+            let newLike = await Like.create({
                 user: req.user._id,
                 likeable: req.query.id,
                 onModel: req.query.type
-            });
 
-            likeable.like.push(like._id);
+            });
+            // console.log(deleted);
+            // Like ki error thi
+            likeable.likes.push(newLike._id);
             likeable.save();
         }
         return res.json(200, {
